ef812bf57fade74ed409d3ccd2da32902f7ea18c
Rtiik Gupta <ritikgupta9414@gmail.com>
2023-04-13 02:51:25 +0530

    nextauth add

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,10 @@
 import { Card, Title, Text } from '@tremor/react';
-import { queryBuilder } from '../lib/planetscale';
 import Search from './search';
-import UsersTable from './table';
 
 export const dynamic = 'force-dynamic';
 
-export default async function IndexPage({
-  searchParams
-}: {
-  searchParams: { q: string };
-}) {
-  const search = searchParams.q ?? '';
-  const users = await queryBuilder
-    .selectFrom('users')
-    .select(['id', 'name', 'username', 'email'])
-    .where('name', 'like', `%${search}%`)
-    .execute();
-
+export default async function IndexPage() {
+  
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Title>Users</Title>
@@ -24,10 +12,6 @@ export default async function IndexPage({
         A list of users retrieved from a MySQL database (PlanetScale).
       </Text>
       <Search />
-      <Card className="mt-6">
-        {/* @ts-expect-error Server Component */}
-        <UsersTable users={users} />
-      </Card>
     </main>
   );
 }
