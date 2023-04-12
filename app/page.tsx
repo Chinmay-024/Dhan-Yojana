3a5fa6b36cb4f7d7c57744e999ac1da176628555
AnkitPatel27 <ankitonfireinwater@gmail.com>
2023-04-13 02:55:43 +0530

    created the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,53 @@
-import { Card, Title, Text } from '@tremor/react';
+import { Card, Title, Text, Flex } from '@tremor/react';
 import { queryBuilder } from '../lib/planetscale';
 import Search from './search';
 import UsersTable from './table';
+import style from './page.module.css';
+
 
 export const dynamic = 'force-dynamic';
-
-export default async function IndexPage({
+export default function IndexPage({
   searchParams
 }: {
   searchParams: { q: string };
 }) {
   const search = searchParams.q ?? '';
-  const users = await queryBuilder
-    .selectFrom('users')
-    .select(['id', 'name', 'username', 'email'])
-    .where('name', 'like', `%${search}%`)
-    .execute();
 
   return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Title>Users</Title>
-      <Text>
-        A list of users retrieved from a MySQL database (PlanetScale).
-      </Text>
-      <Search />
-      <Card className="mt-6">
-        {/* @ts-expect-error Server Component */}
-        <UsersTable users={users} />
-      </Card>
-    </main>
+    <>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Passion+One:wght@700&family=Roboto:wght@100&display=swap');
+      </style>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <main className="p-4 md:p-10 mx-auto max-w-7xl">
+        <div>
+          <h2 className={`text-9xl ${style.heading}`}>Dhan</h2>
+          <h2 className={`text-9xl ${style.heading}`}>Yojana</h2>
+          <Text className="text-2xl text-center">
+            {/* Split the bill, not the friendship */}
+            {/* Split with ease, together or alone, with our app */}
+            Keep track, stay on track
+          </Text>
+        </div>
+        <Flex className={style.container}>
+          <Card className={`w-1/3 m-3 h-80 ${style.card}`}>
+            <Title className='text-center text-2xl'>Manage your expenses on the go with Dhan Yojana </Title>
+            <div><Flex className={`${style.element2}`} justifyContent="center" alignItems="baseline"><span className={`material-symbols-outlined ${style.icon}`}>account_balance</span></Flex>
+            </div>
+          </Card> 
+          <Card className={`w-1/3 m-3 h-80 ${style.card}`}>
+            <Title className='text-center text-2xl'>Keep track of expenses, no matter the currency</Title>
+            <div><Flex className={`${style.element}`} justifyContent="center" alignItems="baseline"><span className={`material-symbols-outlined ${style.icon}`}>currency_exchange</span></Flex>
+            </div>
+          </Card>
+          <Card className={`w-1/3 m-3 h-80 ${style.card}`}> 
+          <Title className='text-center text-2xl'>No more financial headaches,save smarter</Title>
+          <div><Flex justifyContent="center" alignItems="baseline"><span className={`material-symbols-outlined  ${style.icon} ${style.bounce}`}>Savings</span></Flex>
+          </div>
+        </Card>
+        </Flex>
+      </main>
+    </>
   );
 }
