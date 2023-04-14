34383809a8993e598653d0b29bfebc9add0f92e8
Rtiik Gupta <ritikgupta9414@gmail.com>
2023-04-14 22:46:05 +0530

    updates

--- pages/api/user/getAllUser.js
+++ pages/api/user/getAllUser.js
@@ -2,12 +2,10 @@ import { query } from '../../../lib/db';
 
 async function handler(req, res) {
   if (req.method === 'GET') {
-    //TODO make this request get and take groupId as a param
     try {
       const querySql = 'SELECT name,email,image FROM users';
       const valueParams = [];
       const data = await query({ query: querySql, values: valueParams });
-      //TODO Find the money spent on this user by the group
       res.status(200).json({ users: data });
     } catch (error) {
       // unhide to check error
