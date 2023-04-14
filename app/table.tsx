facd31f11daa324add0322c038e1a95b1e6568ac
chinmay <20cs01011@iitbbs.ac.in>
2023-04-14 23:45:29 +0530

    minor changes

--- app/table.tsx
+++ app/table.tsx
@@ -8,7 +8,7 @@ interface User {
 interface Props {
   users: User[];
 }
-import { useState } from "react";
+import { useEffect, useState } from 'react';
 import {
   Table,
   TableBody,
@@ -16,23 +16,29 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  TablePagination,
-} from "@mui/material";
-import * as React from "react";
-import { useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import SkipNextIcon from "@mui/icons-material/SkipNext";
+  TablePagination
+} from '@mui/material';
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
 
 export default function UsersTable({ users }: Props) {
   const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  useEffect(() => {
+    // Initialize state only on the client-side
+    setPage(0);
+    setRowsPerPage(5);
+  }, []);
 
   const handleChangePage = (
     event: React.MouseEvent<HTMLButtonElement> | null,
@@ -66,29 +72,29 @@ export default function UsersTable({ users }: Props) {
 
               <Card
                 sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  width: "100%",
-                  cursor: "pointer",
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  width: '100%',
+                  cursor: 'pointer',
                   background:
-                    "linear-gradient(135deg, #fbfbfc 0%,#f6f7f9 100%)",
-                  margin: "5px",
-                  border: "0px",
-                  borderRadius: "15px",
-                  boxShadow: "none",
+                    'linear-gradient(135deg, #fbfbfc 0%,#f6f7f9 100%)',
+                  margin: '5px',
+                  border: '0px',
+                  borderRadius: '15px',
+                  boxShadow: 'none'
                 }}
               >
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    backgroundColor: "whitesmoke",
-                    flex: "1 0 auto",
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    backgroundColor: 'whitesmoke',
+                    flex: '1 0 auto'
                   }}
                 >
-                  <CardContent sx={{ flex: "1 0 auto" }}>
+                  <CardContent sx={{ flex: '1 0 auto' }}>
                     <Typography component="div" variant="h7">
                       Category
                     </Typography>
@@ -104,13 +110,13 @@ export default function UsersTable({ users }: Props) {
                 </Box>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    flex: "1 0 auto",
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    flex: '1 0 auto'
                   }}
                 >
-                  <CardContent sx={{ flex: "1 0 auto" }}>
+                  <CardContent sx={{ flex: '1 0 auto' }}>
                     <Typography
                       variant="subtitle1"
                       color="text.secondary"
@@ -123,14 +129,14 @@ export default function UsersTable({ users }: Props) {
                 </Box>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "start",
-                    flex: "3 0 auto",
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'start',
+                    flex: '3 0 auto'
                   }}
                 >
-                  <CardContent sx={{ flex: "1 0 auto" }}>
+                  <CardContent sx={{ flex: '1 0 auto' }}>
                     <Typography component="div" variant="h7">
                       {user.name}
                     </Typography>
@@ -138,14 +144,14 @@ export default function UsersTable({ users }: Props) {
                 </Box>
                 <Box
                   sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    backgroundColor: "lightgreen",
-                    flex: "1 0 auto",
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    backgroundColor: 'lightgreen',
+                    flex: '1 0 auto'
                   }}
                 >
-                  <CardContent sx={{ flex: "1 0 auto" }}>
+                  <CardContent sx={{ flex: '1 0 auto' }}>
                     <Typography component="div" variant="h7">
                       You Owe
                     </Typography>
